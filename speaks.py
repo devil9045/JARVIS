0b76096e10735229f5cc968fbee83207acc42423
devil9045 <65858468+devil9045@users.noreply.github.com>
2020-06-17 07:40:58 +0530

    Update speaks.py

--- speaks.py
+++ speaks.py
@@ -7,25 +7,25 @@ import datetime
 
 def process_text(text):
     try:
-        if 'web' in text or 'play' in text:
+        if 'web' in text.lower() or 'play' in text.lower():
             search.search_web(text)
 
-        elif "who are you" in text or "define yourself" in text:
+        elif "who are you" in text.lower() or "define yourself" in text.lower():
             speak = 'HELLO I AM JARVIS YOUR PERSONAL ASSISTANT'
             assistant_speakes(speak)
 
-        elif "who made you" in text or "created you" in text:
+        elif "who made you" in text.lower() or "created you" in text.lower():
             speak = "I have been created by master Henish."
             assistant_speakes(speak)
 
-        elif 'open' in text:
+        elif 'open' in text.lower():
             calc.open_application(text)
 
-        elif 'date' in text or 'today date' in text:
+        elif 'date' in text.lower() or 'today date' in text.lower():
             date = datetime.datetime.date().today()
             assistant_speakes(date)
 
-        elif 'time' in text or 'current time' in text or 'what is time' in text:
+        elif 'time' in text.lower() or 'current time' in text.lower() or 'what is time' in text.lower():
             time = datetime.datetime.today().time().replace(microsecond=0)
             assistant_speakes(time)
 
